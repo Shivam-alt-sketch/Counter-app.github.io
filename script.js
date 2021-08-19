bf44ca1f30bd3d06d54023db9dec71d2d947fe52
Shivam Pakade <64470228+Shivam-alt-sketch@users.noreply.github.com>
2021-08-19 08:36:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//const add = document.querySelector(".add");
+//const resetCount = document.querySelector(".reset");
+//const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
+const count = document.querySelector(".count");
+
+buttons.addEventListener("click", (e) => {
+    if(e.target.classList.contains("add")){
+        count.innerHTML++;
+    }
+    if(e.target.classList.contains("subtract")){
+        count.innerHTML--;
+    }
+    if(e.target.classList.contains("reset")){
+        count.innerHTML = 0;
+    }
+});
+
+
+
+
+
+
+
+
+/*add.addEventListener("click", ()=>{
+    count.innerHTML++;
+});
+sub.addEventListener("click", ()=>{
+    count.innerHTML--;
+});
+resetCount.addEventListener("click", ()=>{
+    count.innerHTML = 0;
+});*/
